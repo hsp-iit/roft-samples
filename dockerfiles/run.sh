77fa777b05c74ebe875cff48b3ebb6f4e9ec09e8
Nicola Piga <nicolapiga@gmail.com>
2022-11-02 14:54:26 +0000

    Better indent lines in dockerfiles/run.sh

--- dockerfiles/run.sh
+++ dockerfiles/run.sh
@@ -1,1 +1,10 @@
-docker run -it --privileged --name roft-samples-cnt --user user --env="DISPLAY" --net=host -e NVIDIA_DRIVER_CAPABILITIES=all -v /tmp/.X11-unix:/tmp/.X11-unix --device /dev/dri/ --volume=/dev:/dev --gpus all --runtime=nvidia roft-samples-img
+docker run \
+       -it --privileged \
+       --name roft-samples-cnt \
+       --user user \
+       --net=host \
+       -e DISPLAY=$DISPLAY -e NVIDIA_DRIVER_CAPABILITIES=all \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -v /dev:/dev \
+       --gpus all --runtime=nvidia \
+       roft-samples-img
